05ff11cae3729b1b8a6c0c093df50e631a57cb56
Zyad Yasser <zyadyasser6@gmail.com>
2020-05-11 22:08:27 +0200

    Add native social login

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -4,22 +4,44 @@
     <ion-content class="ion-padding">
       <ion-button v-on:click="navigator()" color="primary">Back</ion-button>
       <br />
-      <ion-button v-on:click="openCamera()" color="primary">Open camera</ion-button>
+      <ion-button v-on:click="loginFacebook()" color="primary">Login with Facebook</ion-button>
       <br />
-      <ion-button v-on:click="openMedia()" color="primary">Open media</ion-button>
+      <ion-button v-on:click="loginTwitter()" color="primary">Login with Twitter</ion-button>
     </ion-content>
   </div>
 </template>
 
 <script>
+import { Plugins } from '@capacitor/core';
+import { FacebookLoginResponse } from '@rdlabo/capacitor-facebook-login';
+import { Twitter } from 'capacitor-twitter';
+const { FacebookLogin } = Plugins;
+
 export default {
   name: "profile",
   methods: {
-    openMedia() {
+    async loginFacebook() {
+      try {
+        const FACEBOOK_PERMISSIONS = ['email', 'user_birthday', 'user_photos', 'user_gender'];
+        const result = await FacebookLogin.login({ permissions: FACEBOOK_PERMISSIONS });
 
+        if (result.accessToken) {
+          console.log(`Facebook access token is ${result.accessToken.token}`);
+        } else {
+          // Cancelled by user.
+        } 
+      } catch (error) {
+        console.log(error);
+      }
     },
-    openCamera() {
-
+    async loginTwitter() {
+      try {
+        const twitter = new Twitter();
+        console.log(twitter)
+        const res = await twitter.login();
+      } catch (error) {
+        console.log(error);
+      }
     },
     navigator() {
       this.$router.go(-1)
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -9,14 +9,14 @@ import { API_URL } from '@/config'
 export class AuthService {
   static async makeLogin ({ email, password }) {
     try {
-      const response = await axios.post(`${API_URL}/auth/login`,
-        { email, password, fingerprint },
-        { withCredentials: true })
-      _setAuthData({
-        accessToken: response.data.data.accessToken,
-        exp: _parseTokenData(response.data.data.accessToken).exp
-      })
-      return new ResponseWrapper(response, response.data.data)
+      // const response = await axios.post(`${API_URL}/auth/login`,
+      //   { email, password, fingerprint },
+      //   { withCredentials: true })
+      // _setAuthData({
+      //   accessToken: response.data.data.accessToken,
+      //   exp: _parseTokenData(response.data.data.accessToken).exp
+      // })
+      // return new ResponseWrapper(response, response.data.data)
     } catch (error) {
       throw new ErrorWrapper(error)
     }
@@ -25,7 +25,7 @@ export class AuthService {
   static async makeLogout () {
     try {
       const response = await new Http({ auth: true }).post('auth/logout', {}, { withCredentials: true })
-      _resetAuthData()
+      // _resetAuthData()
       $router.push({ name: 'login' }).catch(() => {})
       return new ResponseWrapper(response, response.data.data)
     } catch (error) {
